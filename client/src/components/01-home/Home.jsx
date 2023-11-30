bbc4cd3f5acb18befd3aa38fa97a5555b0ce488b
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-11-30 05:55:22 +0300

    Add server

--- client/src/components/01-home/Home.jsx
+++ client/src/components/01-home/Home.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { Spot } from "@binance/connector";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Home = () => {
+  const [balance, setBalance] = useState(null);
+  const apiKey = import.meta.env.VITE_binanceApiKey;
+  const apiSecret = import.meta.env.VITE_binanceApiSecretKey;
+  const client = new Spot(apiKey, apiSecret);
+
+  useEffect(() => {
+    client
+      .marginAccount()
+      .then((response) => client.logger.log(response.data))
+      .catch((error) => client.logger.error(error));
+  }, []);
+
   return (
     <div className="home-container">
       <Header />
