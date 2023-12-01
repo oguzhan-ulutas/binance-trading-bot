3ecc52cc2db4ae584d170c5ceafcf07eb9c02632
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-12-01 21:29:44 +0300

    Add controller to back end

---
+++ server/controllers/binanceController.js
@@ -0,0 +1,5 @@
+const asyncHandler = require('express-async-handler');
+
+exports.getBalance = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Display single post detail');
+});
--- server/routes/binanceApiV1.js
+++ server/routes/binanceApiV1.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const binanceController = require('../controllers/binanceController');
+
+// Balance Routes
+// Get balance
+router.get('/balance', binanceController.getBalance);
+
 module.exports = router;
