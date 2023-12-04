d17ec6ae0da31aee5707af7550068fb02fc5816d
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-12-04 21:04:30 +0300

    Add calculating trade size logic

--- client/src/components/03-order/TotalRiskCalculator.jsx
+++ client/src/components/03-order/TotalRiskCalculator.jsx
@@ -4,6 +4,8 @@ import { outlinedInputClasses } from "@mui/material/OutlinedInput";
 import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
 
+import Divider from "@mui/material/Divider";
+
 import { BotContext } from "../BotContext";
 
 const customTheme = (outerTheme) =>
@@ -82,52 +84,108 @@ export default function TotalRiskCalculator() {
   const [marginBalance, setMArginBalance] = React.useState(balance);
   const [riskPercent, setRiskPercent] = React.useState(5);
   const [riskUsdt, setRiskUsdt] = React.useState(0);
+  const [tradeRiskPercent, setTradeRiskPercent] = React.useState(5);
+  const [tradeSize, setTradeSize] = React.useState(0);
 
   const calculateMaxRisk = (totalBalance, risk) => {
     const maxRisk = Number(totalBalance) * (Number(risk) * 0.01);
     setRiskUsdt(maxRisk);
   };
 
-  // Run calculateMaxRisk on load
+  // Calculate calculateMaxRisk
   React.useEffect(() => {
     calculateMaxRisk(marginBalance, riskPercent);
   }, [marginBalance, riskPercent]);
 
+  const calculateMaxTradeSize = (totalMaxRisk, tradeRisk) => {
+    console.log((totalMaxRisk / tradeRisk) * 0.01);
+
+    const maxRisk = Number(totalMaxRisk) / (Number(tradeRisk) * 0.01);
+    console.log(maxRisk);
+    setTradeSize(maxRisk);
+  };
+
+  React.useEffect(() => {
+    calculateMaxTradeSize(riskUsdt, tradeRiskPercent);
+  }, [riskUsdt, tradeRiskPercent]);
+
   return (
     <>
-      <h3>Maximum Risk</h3>
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: { sm: "1fr 1fr 1fr" },
-          gap: 2,
-        }}
-      >
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField
-            label="Margin Balance(usdt)"
-            variant="standard"
-            defaultValue={balance.toFixed(2)}
-            onChange={(e) => setMArginBalance(e.target.value)}
-          />
-          <TextField
-            label="Risk(%)"
-            variant="standard"
-            defaultValue={5}
-            onChange={(e) => {
-              setRiskPercent(e.target.value);
-            }}
-          />
-          <TextField
-            label="Max Lost(usdt)"
-            variant="standard"
-            value={riskUsdt.toFixed(2)}
-          />
-        </ThemeProvider>
-      </Box>
-      <p>{`If you take ${riskPercent}% risk on your all balance, you will lose ${riskUsdt.toFixed(
-        2
-      )} usdt.`}</p>
+      <div className="max-risk-usdt">
+        <h3>Maximum Risk USDT</h3>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: { sm: "1fr 1fr 1fr" },
+            gap: 2,
+          }}
+        >
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Margin Balance(usdt)"
+              variant="standard"
+              defaultValue={balance.toFixed(2)}
+              onChange={(e) => setMArginBalance(Number(e.target.value))}
+            />
+            <TextField
+              label="Risk(%)"
+              variant="standard"
+              defaultValue={5}
+              onChange={(e) => {
+                setRiskPercent(Number(e.target.value));
+              }}
+            />
+            <TextField
+              label="Max Lost(usdt)"
+              variant="standard"
+              value={riskUsdt.toFixed(2)}
+            />
+          </ThemeProvider>
+        </Box>
+        <p>{`If you take ${riskPercent}% risk on your all balance, you will lose ${riskUsdt.toFixed(
+          2
+        )} usdt.`}</p>
+      </div>
+
+      <Divider />
+
+      <div className="trade-size">
+        <h3>Trade Size</h3>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: { sm: "1fr 1fr 1fr" },
+            gap: 2,
+          }}
+        >
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Max Risk(usdt)"
+              variant="standard"
+              value={riskUsdt.toFixed(2)}
+              onChange={(e) => setRiskUsdt(Number(e.target.value))}
+            />
+            <TextField
+              label="Trade Risk(%)"
+              variant="standard"
+              defaultValue={5}
+              onChange={(e) => {
+                setTradeRiskPercent(Number(e.target.value));
+              }}
+            />
+            <TextField
+              label="Trade Size(usdt)"
+              variant="standard"
+              value={tradeSize.toFixed(2)}
+            />
+          </ThemeProvider>
+        </Box>
+        <p>{`If you ok with losing ${riskUsdt.toFixed(
+          2
+        )} usdt and want to open a new trade with ${tradeRiskPercent}% risk, you can open a new trade with max-size of ${tradeSize.toFixed(
+          2
+        )} usdt.`}</p>
+      </div>
     </>
   );
 }
